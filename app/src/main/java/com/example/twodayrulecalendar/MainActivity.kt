dcbb61aee79b8d3cba12a9a3d4c360638dab8047
BorisBBoychev <borisboychev007@Gmail.com>
2020-08-27 20:43:18 +0300

    adding text views programmatically

--- app/src/main/java/com/example/twodayrulecalendar/MainActivity.kt
+++ app/src/main/java/com/example/twodayrulecalendar/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.twodayrulecalendar
 
+import android.app.ActionBar
 import android.app.DatePickerDialog
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.*
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.squareup.timessquare.CalendarPickerView
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
+@Suppress("PLUGIN_WARNING")
 class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
     private var textView: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,15 +22,15 @@ class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
         val dateToday = Date()
         val calendar : CalendarView = findViewById(R.id.calendar_grid)
 
-        textView = findViewById(R.id.tv)
 
+        //TODO add todos (text views) programmatically on every date pick
         fab_btn.setOnClickListener{
             showDatePicker()
         }
     }
 
-    fun showDatePicker(){
-        var dpd: DatePickerDialog = DatePickerDialog(
+    private fun showDatePicker(){
+        val dpd: DatePickerDialog = DatePickerDialog(
             this,this,  Calendar.getInstance().get(Calendar.YEAR)
             ,Calendar.getInstance().get(Calendar.MONTH)
             , Calendar.getInstance().get(Calendar.DAY_OF_MONTH))
@@ -35,7 +39,13 @@ class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
     override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
         val date: String = "dd/mm/yy $day/$month/$year"
-        textView?.text = date
+        val newTv: TextView = TextView(this)
+        newTv.textSize = 20f
+        newTv.setTextColor(Color.parseColor("#000000"))
+        newTv.text = date
+        newTv.layoutParams = ViewGroup.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT)
+        val ll: LinearLayout = findViewById(R.id.ll_for_todos)
+        ll.addView(newTv)
     }
 }
 
