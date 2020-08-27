a54f35258f26c4b5b7361680bf64d16c3aa56537
BorisBBoychev <borisboychev007@Gmail.com>
2020-08-27 20:19:39 +0300

    date picker

--- app/src/main/java/com/example/twodayrulecalendar/MainActivity.kt
+++ app/src/main/java/com/example/twodayrulecalendar/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.twodayrulecalendar
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AdapterView
-import android.widget.CalendarView
-import android.widget.Toast
+import android.widget.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.squareup.timessquare.CalendarPickerView
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
+    private var textView: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,8 +18,24 @@ class MainActivity : AppCompatActivity() {
         val dateToday = Date()
         val calendar : CalendarView = findViewById(R.id.calendar_grid)
 
-        calendar.setOnDateChangeListener { calendarView, y, m, d ->
-            Toast.makeText(this, "$y, $m, $d", Toast.LENGTH_SHORT).show()
+        textView = findViewById(R.id.tv)
+
+        fab_btn.setOnClickListener{
+            showDatePicker()
         }
     }
+
+    fun showDatePicker(){
+        var dpd: DatePickerDialog = DatePickerDialog(
+            this,this,  Calendar.getInstance().get(Calendar.YEAR)
+            ,Calendar.getInstance().get(Calendar.MONTH)
+            , Calendar.getInstance().get(Calendar.DAY_OF_MONTH))
+        dpd.show()
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
+        val date: String = "dd/mm/yy $day/$month/$year"
+        textView?.text = date
+    }
 }
+
